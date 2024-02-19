5c7346e54d5a9d4ada359bb6193860ab8adb86d9
Javier Villagomez <javier_diaz16@hotmail.com>
2024-02-19 13:26:40 -0600

    ci:funcion saludar persona

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hola, Ch38");
+
+function saludoAlert() {
+    alert("Hola, bienvenido");
+}
+
+function enviarSaludoAPersona() {
+    let nombre = prompt("¿Cuál es tu nombre?");
+    alert(`Hola, ${nombre}`);
+}
+
+saludoAlert();
